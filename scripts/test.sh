2820cb0528395f212ba00318ae073bc8073568be
David Lemarier <david@reallysuccessful.com>
2013-09-13 07:49:20 -0400

    Change travis.ylm

---
+++ scripts/test.sh
@@ -0,0 +1,116 @@
+#!/bin/sh
+
+set -e
+
+DB_HOST='localhost'
+DB_NAME='leadrush_dev'
+DB_USER='root'
+DB_PASSWORD=''
+URL='http://leadrush-ci/'
+SCREENSHOT=screenshot/
+REPORT=report
+CASPERJS=ci/vendor/casperjs/bin/casperjs
+
+DATE=$(date +"%F-%H-%M-%S")
+SCREENSHOTDATE="${SCREENSHOT}${DATE}-"
+CASPER_OPTIONS="--fail-fast --direct --log-level=warning --capture_path=$SCREENSHOTDATE --includes=./ci/tests/casperjs/pre.js --base_url=$URL"
+
+help ()
+{
+    echo "Test suite for Leadrush LTD"
+    echo ""
+    echo "    test init             - initialise Novius OS test instance";
+    echo "    test install [wizard] - runs begin of tests suite, install and appmanager";
+    echo "    test run [stepname]   - runs complete tests suite";
+}
+
+db ()
+{
+    echo "Init DB"
+	$(mysql -h $DB_HOST -u $DB_USER --password=$DB_PASSWORD -e "DROP DATABASE IF EXISTS $DB_NAME;CREATE DATABASE $DB_NAME DEFAULT CHARACTER SET utf8 COLLATE utf8_general_ci;")
+}
+
+init ()
+{
+	db
+
+    echo "Instance Novius OS test reset"
+	cd $ROOT
+
+	sudo rm -rf $REPORT
+	sudo rm -rf $SCREENSHOT
+	sudo rm -rf local/config/development
+	sudo rm -f local/config/db.config.php
+	sudo rm -f local/config/crypt.config.php
+	sudo rm -f local/config/contexts.config.php
+	sudo rm -f local/config/contexts.config.php
+	sudo rm -rf local/metadata/*
+	sudo rm -rf local/cache/fuelphp/*
+	sudo rm -rf local/data/media/*
+	sudo rm -rf local/cache/media/*
+	sudo rm -rf local/cache/fuelphp/*
+	sudo rm -rf local/data/config/*
+	sudo rm -rf public/cache/media/*
+	sudo rm -rf public/media/*
+
+	echo "Instance Novius OS test reseted"
+}
+
+install ()
+{
+    set -e
+
+    echo "Test install begin"
+	cd $ROOT
+
+	$CASPERJS test ./ci/tests/casperjs/install.js --xunit=$REPORT/casper-install.xml $CASPER_OPTIONS --host="$DB_HOST" --user="$DB_USER" --password="$DB_PASSWORD" --db=$DB_NAME
+
+    if [ "$1" = "wizard" ]; then return; fi
+
+    $CASPERJS test ./ci/tests/casperjs/appmanager.js --xunit=$REPORT/casper-appmanager.xml $CASPER_OPTIONS
+}
+
+run ()
+{
+    set -e
+    echo "Test begin"
+	cd $ROOT
+
+	install
+
+    wget http://www.novius-os.org/static/apps/noviusos_templates_basic/img/logo.png -O /tmp/logo-novius-os.png
+
+    if [ -n $1 ]
+    then
+        CASPER_OPTIONS="$CASPER_OPTIONS --nos_step=$1"
+    fi
+
+    $CASPERJS test ./ci/tests/casperjs/scenario.js --xunit=$REPORT/casper-scenario.xml $CASPER_OPTIONS
+}
+
+ROOT=$(pwd)/
+
+if [ $1 ]
+then
+    case "$1" in
+		"init")
+			init
+			exit $?
+		;;
+		"install")
+			init
+			install  $2
+			exit $?
+		;;
+		"run")
+			init
+			run $2
+			exit $?
+		;;
+		*)
+			help
+		;;
+	esac
+else
+    help
+fi
---
+++ scripts/travis.sh
@@ -0,0 +1,58 @@
+#! /bin/sh
+
+cd ../../
+
+run ()
+{
+    if [ "$1" = 'local' ]
+    then
+        echo "Test suite local"
+        ci/scripts/test.sh run
+        return $?
+    else
+        export PHANTOMJS_EXECUTABLE='phantomjs --local-to-remote-url-access=yes --ignore-ssl-errors=yes'
+        export DISPLAY=:99.0
+        sh -e /etc/init.d/xvfb start
+
+        DISPLAY=:99.0 ci/scripts/test.sh run
+        return $?
+    fi
+}
+
+echo "Test suite begin"
+if [ "$1" != 'local' ]
+    then
+    cd ci/scripts
+    ./init.sh
+    cd ../../
+fi
+run
+temp=$?
+
+if [ $temp != 0  -a "$1" != 'local' ]
+    then
+    cd ci/scripts
+    ./init.sh
+    cd ../../
+    run
+    temp=$?
+fi
+
+echo "Test suite end : $temp"
+
+if [ $temp != 0  -a "$1" != 'local' ]
+then
+
+    if [ -d screenshot ]
+    then
+        IMAGESHACK_DEVELOPER_KEY=8ABCDELQ673cd7e375ad15fa94a10c45b9a699f9
+        export IMAGESHACK_DEVELOPER_KEY
+
+        for file in screenshot/*
+        do
+            echo "Send $file to imageshack"
+            ci/vendor/imageshack-upload -i "$file"
+        done
+    fi
+fi
+exit $temp
